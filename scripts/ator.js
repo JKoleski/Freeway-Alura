5fdbcc62e65ce360819c9337b91c8855a6f6f044
JKoleski <joaokoleski@gmail.com>
2022-12-20 18:12:49 -0300

    Verificação de colisão e mais carros

--- scripts/ator.js
+++ scripts/ator.js
@@ -1,4 +1,4 @@
-let eixoXAtor=100 ,eixoYAtor=366;
+let eixoXAtor=100 ,eixoYAtor=366, colisao=false;
 
 function geraAtor(){
     image(imagemAtor, eixoXAtor, eixoYAtor, 30, 30);
@@ -9,4 +9,18 @@ function movimentaAtor(){
         eixoYAtor -= 3;
     if(keyIsDown(DOWN_ARROW)&&eixoYAtor+30<400)
         eixoYAtor += 3;
+}
+
+function verificaColisao(){
+    //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+    for(let i=0;imagemCarros.length > i; i++){
+        colisao = collideRectCircle(eixoXCarros[i],eixoYCarros[i],comprimentoCarro,alturaCarro,eixoXAtor,eixoYAtor,15);
+        if(colisao){
+            colidiu();
+        }
+    }
+}
+
+function colidiu(){
+    eixoYAtor=366;
 }
--- scripts/carro.js
+++ scripts/carro.js
@@ -1,8 +1,8 @@
-let eixoYCarros=[40,96,150], eixoXCarros=[600,600,600], velocidadeCarros=[2,2.5,3.2];
-
+let eixoYCarros=[40,96,150, 210,270,318], eixoXCarros=[600,600,600,600,600,600], velocidadeCarros=[2,2.5,3.2,5,3.3,2.3];
+let comprimentoCarro=50, alturaCarro=40;
 function geraCarro(){
     for(let i=0;imagemCarros.length>i;i++)
-        image(imagemCarros[i], eixoXCarros[i], eixoYCarros[i], 50, 40);
+        image(imagemCarros[i], eixoXCarros[i], eixoYCarros[i], comprimentoCarro, alturaCarro);
 }
   
 function movimentaCarro(){
